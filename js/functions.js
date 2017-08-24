ae507f22cd447429c243b921949543e5f48f836f
Noahdelagardelle <noahcdela@gmail.com>
2017-08-24 12:09:31 -0500

    Add favicons and finish optimization

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,63 @@
 $(function(){
 
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
   $('.nav-toggle').click(function(){
     $('nav ul').toggleClass('on');
   });
 
   $('a, section, footer').on('click', function(){
     $('nav ul').removeClass('on');
-  })
+  });
 
+  $(window).scroll(function(){
+
+    var scroll = $(this).scrollTop();
+
+    $('.logo').css({
+      'transform' : 'translate(0px, '+ scroll /4 +'%)'
+    });
+
+    if (scroll >= 100) {
+      $('nav').addClass('scrolled');
+      $('nav ul').addClass('ul-scrolled');
+    } else {
+      $('nav').removeClass('scrolled');
+      $('nav ul').removeClass('ul-scrolled');
+    }
+
+  });
+
+  // Flickity
+  $('.lyrics-carousel').flickity({
+    cellAlign: 'left',
+    contain: true,
+    arrowShape: { 
+      x0: 10,
+      x1: 60, y1: 50,
+      x2: 60, y2: 40,
+      x3: 20
+    }
+  });
+
+  $('.photos-carousel').flickity({
+    wrapAround: true,
+    imagesLoaded: true,
+    percentPosition: false,
+    initialIndex: 2,
+    arrowShape: {
+      x0: 15,
+      x1: 60, y1: 45,
+      x2: 70, y2: 45,
+      x3: 25
+    }
+  });
 });
 
 
